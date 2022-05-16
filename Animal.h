1c25af4e1c424bfd9ae66bf1a687036e844917ab
Maryia <83656825+kottarainen@users.noreply.github.com>
2022-05-16 23:19:37 +0300

    update animal.h

--- Animal.h
+++ Animal.h
@@ -7,23 +7,34 @@
 #include <vector>
 #include <unordered_set>
 
-class Animal {
+class Animal;
+
+class IAnimal {
+public:
+    virtual std::string GetName() = 0;
+    virtual std::vector<IAnimal*> GetFriends() = 0;
+
+    virtual std::string Type() = 0;
+    virtual  bool operator ==(const Animal& other) = 0;
+    virtual  bool operator < (const Animal& other) = 0;
+    virtual  void AddFriend(IAnimal* other) = 0;
+};
+
+class Animal : public IAnimal{
 public:
     explicit Animal(std::string  name) : name_(std::move(name)) {};
 
-    std::string GetName();
+    std::string GetName() override;
 
-    std::vector<std::string> GetFriends();
+    std::vector<IAnimal*> GetFriends() override;
 
-    virtual std::string Type();
-    bool operator ==(const Animal& other);
-    bool operator < (const Animal& other);
-    void AddFriend(Animal other);
-
+    bool operator ==(const Animal& other) override;
+    bool operator < (const Animal& other) override;
+    void AddFriend(IAnimal* other) override;
+    std::string Type() override;
 
 protected:
-    std::vector<Animal> friends_;
+    std::vector<IAnimal*> friends_;
     std::string name_;
 };
-
 #endif //COURSEWORK_ANIMAL_H
